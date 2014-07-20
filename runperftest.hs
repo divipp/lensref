c85d3010f814247032543de4fd54b830f7ee5051
divip <divipp@gmail.com>
2014-07-20 09:30:32 +0200

    refactoring: move performance test definitions

--- runperftest.hs
+++ runperftest.hs
@@ -1,17 +1,14 @@
-{-# LANGUAGE CPP #-}
+--{-# LANGUAGE RankNTypes #-}
 
-#ifndef __TESTS__
-main :: IO ()
-main = fail "enable the tests flag"
-#else
 import Data.IORef
 import Control.Monad
 import Criterion.Main
 import Criterion.Config
 
-import Data.LensRef.Test
+import Data.LensRef.SimpleRef
+import Data.LensRef
 
-------------------
+--------------------------------------------------------------------------------
 
 myConfig = defaultConfig
               -- Always GC between runs.
@@ -33,6 +30,8 @@ main = defaultMainWith myConfig (return ())
   where
     range = [20000,40000,60000]
 
+--------------------------------------------------------------------------------
+
 -- for comparison
 ioRefTest n = do
     rs <- replicateM n $ newIORef 'x'
@@ -44,4 +43,59 @@ ioRefTest n = do
 
     a ==? b | a == b = return ()
     a ==? b = fail $ show a ++ " /= " ++ show b
-#endif
+
+--------------------------------------------------------------------------------
+
+runPerformanceTests :: String -> Int -> IO ()
+runPerformanceTests name n = do
+
+    let a ==? b = when (a /= b) $ fail $ show a ++ " /= " ++ show b
+
+        r ==> v = readerToWriter $ readRef r >>= (==? v)
+
+    join $ runRefCreator $ \runRefWriter -> fmap runRefWriter $ case name of
+        "create" -> do
+            rs <- replicateM n $ newRef 'x'
+            return $ do
+                forM_ rs $ \r -> r ==> 'x'
+                forM_ rs $ \r -> writeRef r 'y'
+                forM_ rs $ \r -> r ==> 'y'
+
+        "mapchain" -> do
+            r <- newRef 'x'
+            let q = iterate (lensMap id) r !! n
+            return $ do
+                q ==> 'x'
+                writeRef q 'y'
+                q ==> 'y'
+
+        "joinchain" -> do
+            rb <- newRef True
+            r1 <- newRef 'x'
+            r2 <- newRef 'y'
+            let f (r1, r2) = (r1', r2') where
+                    r1' = joinRef $ readRef rb <&> \b -> if b then r1 else r2
+                    r2' = joinRef $ readRef rb <&> \b -> if b then r2 else r1
+                (r1', r2') = iterate f (r1, r2) !! (2*n)
+            return $ do
+                r1' ==> 'x'
+                r2' ==> 'y'
+                writeRef r1' 'X'
+                r1' ==> 'X'
+                r2' ==> 'y'
+                writeRef r2' 'Y'
+                r1' ==> 'X'
+                r2' ==> 'Y'
+                writeRef rb False
+                r1' ==> 'X'
+                r2' ==> 'Y'
+                writeRef r1' 'x'
+                r1' ==> 'x'
+                r2' ==> 'Y'
+                writeRef r2' 'y'
+                r1' ==> 'x'
+                r2' ==> 'y'
+
+        _ -> error $ "No such test: " ++ name
+
+
--- src/Data/LensRef/Test.hs
+++ src/Data/LensRef/Test.hs
@@ -1,13 +1,9 @@
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 -- | Tests for the lens references interface.
 module Data.LensRef.Test
     ( -- * Tests for the interface
       runTests
-    , runPerformanceTests
     ) where
 
 import Data.Maybe
@@ -837,58 +833,6 @@ runTests = do
 
     return ()
 
-
-runPerformanceTests :: String -> Int -> IO ()
-runPerformanceTests name n = do
-
-    let a ==? b = when (a /= b) $ fail $ show a ++ " /= " ++ show b
-
-        r ==> v = readerToWriter $ readRef r >>= (==? v)
-
-    join $ runRefCreator $ \runRefWriter -> fmap runRefWriter $ case name of
-        "create" -> do
-            rs <- replicateM n $ newRef 'x'
-            return $ do
-                forM_ rs $ \r -> r ==> 'x'
-                forM_ rs $ \r -> writeRef r 'y'
-                forM_ rs $ \r -> r ==> 'y'
-
-        "mapchain" -> do
-            r <- newRef 'x'
-            let q = iterate (lensMap id) r !! n
-            return $ do
-                q ==> 'x'
-                writeRef q 'y'
-                q ==> 'y'
-
-        "joinchain" -> do
-            rb <- newRef True
-            r1 <- newRef 'x'
-            r2 <- newRef 'y'
-            let f (r1, r2) = (r1', r2') where
-                    r1' = joinRef $ readRef rb <&> \b -> if b then r1 else r2
-                    r2' = joinRef $ readRef rb <&> \b -> if b then r2 else r1
-                (r1', r2') = iterate f (r1, r2) !! (2*n)
-            return $ do
-                r1' ==> 'x'
-                r2' ==> 'y'
-                writeRef r1' 'X'
-                r1' ==> 'X'
-                r2' ==> 'y'
-                writeRef r2' 'Y'
-                r1' ==> 'X'
-                r2' ==> 'Y'
-                writeRef rb False
-                r1' ==> 'X'
-                r2' ==> 'Y'
-                writeRef r1' 'x'
-                r1' ==> 'x'
-                r2' ==> 'Y'
-                writeRef r2' 'y'
-                r1' ==> 'x'
-                r2' ==> 'y'
-
-        _ -> error $ "No such test: " ++ name
 
 -------------------------- auxiliary definitions
 
